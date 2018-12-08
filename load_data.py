4aea7951424179e2af6ec14c4c89617e6e66a649
Eli <epugh@stanford.edu>
2018-12-08 15:18:35 -0800

    first

--- load_data.py
+++ load_data.py
@@ -12,4 +12,18 @@ def loadall(filename=''):
             'x_te' : x_te, 'y_te' : y_te,
             'x_cv' : x_cv, 'y_cv' : y_cv, }
 
-# print(loadall('melspects.npz')['x_tr'].shape)
+###########################################
+
+# Use this with:
+
+###########################################
+# import load_data
+
+# data = load_data.loadall('melspects.npz')
+# x_tr = data['x_tr']
+# y_tr = data['y_tr']
+# x_te = data['x_te']
+# y_te = data['y_te']
+# x_cv = data['x_cv']
+# y_cv = data['y_cv']
+###########################################
